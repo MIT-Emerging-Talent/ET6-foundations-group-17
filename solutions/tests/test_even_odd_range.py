9ba9be7fd97541ebba3bf04ec1c74978e9f5eb9d
Rouaa93 <rouaa.hmz@gmail.com>
2025-01-13 00:48:41 +0200

    Update test_even_odd_range.py

--- solutions/tests/test_even_odd_range.py
+++ solutions/tests/test_even_odd_range.py
@@ -1,12 +1,9 @@
 import unittest
 
-try:
-    # For package execution
-    from ..even_odd_range import even_odd_range
-except ImportError:
-    # For standalone execution
-    from even_odd_range import even_odd_range
-
+import sys
+from pathlib import Path
+sys.path.append(str(Path(file).parent.parent))
+from even_odd_range import even_odd_range
 
 class TestEvenOddRange(unittest.TestCase):
     """
